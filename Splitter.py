840c50cebbcc375967467dbe39c0e35eaa609a58
mlovelace713 <mlovelace713@gmail.com>
2017-10-01 22:09:48 -0400

    Create Splitter.py

---
+++ Splitter.py
@@ -0,0 +1,7 @@
+with open(‘structures.sdf’) as f:
+    n = 0
+    data = f.read()
+    entries = data.split(‘$$$$$’) #This line is between each molecule file
+    for x in entries
+        output.write(“drugfile” % x)
+        
