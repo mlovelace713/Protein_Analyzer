3bfc594d3c2a783dcbe28c21d23d1b265675b4b8
Brian Osborne <briano@bioteam.net>
2017-10-04 09:39:49 -0400

    PEP8 format

--- Splitter.py
+++ Splitter.py
@@ -1,7 +1,6 @@
 with open(‘structures.sdf’) as f:
     n = 0
     data = f.read()
-    entries = data.split(‘$$$$$’) #This line is between each molecule file
+    entries = data.split(‘$$$$$’)  # This line is between each molecule file
     for x in entries
         output.write(“drugfile” % x)
-        
