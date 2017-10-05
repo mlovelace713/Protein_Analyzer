28adc0fcaa0de8078514c082c9699bbbe949f7b5
mlovelace713 <mlovelace713@gmail.com>
2017-10-04 23:15:11 -0400

    Update Splitter.py
    Error Message I am Getting:
    Traceback (most recent call last):
      File "splitter.py", line 3, in <module>
        with open("Users/mattlovelace/Desktop/structures.sdf") as f:
    IOError: [Errno 2] No such file or directory: 'Users/mattlovelace/Desktop/structures.sdf'

--- Splitter.py
+++ Splitter.py
@@ -4,6 +4,6 @@ with open("Users/mattlovelace/Desktop/structures.sdf") as f:
     n = 0
     data = f.read()
     entries = data.split("$$$$$") #This line is between each molecule file
-    for x in entries:
+    for x in entries
         output.write(“drugfile” % x)
         
