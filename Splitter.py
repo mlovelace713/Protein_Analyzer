be4c8d57d5c97eb07ded4b104deb63faf4694145
mlovelace713 <mlovelace713@gmail.com>
2017-10-04 23:16:21 -0400

    Update Splitter.py

--- Splitter.py
+++ Splitter.py
@@ -4,6 +4,11 @@ with open("Users/mattlovelace/Desktop/structures.sdf") as f:
     n = 0
     data = f.read()
     entries = data.split("$$$$$") #This line is between each molecule file
-    for x in entries
+    for x in entries:
         output.write(“drugfile” % x)
         
+# I am getting the following error message: 
+# Traceback (most recent call last):
+#   File "splitter.py", line 3, in <module>
+#     with open("Users/mattlovelace/Desktop/structures.sdf") as f:
+# IOError: [Errno 2] No such file or directory: 'Users/mattlovelace/Desktop/structures.sdf'
