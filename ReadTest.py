6a16d3201e095190c5015200a8c332a5b11657c8
Brian Osborne <briano@bioteam.net>
2017-10-23 09:37:12 -0400

    Formatting

--- ReadTest.py
+++ ReadTest.py
@@ -3,6 +3,7 @@
 with open("/Users/mattlovelace/Desktop/Drug_Structures/structures_nutraceutical.sdf") as f:
     data = f.read()
     items = data.split('$$$$')
+
 for item in items:
     n = item.find('> <DATABASE_ID>')
     if (n != -1):
