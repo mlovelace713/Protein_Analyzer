ee49a417b710c489ec677c89dd4224d4ef0582fc
mlovelace713 <mlovelace713@gmail.com>
2017-11-05 00:56:29 -0400

    Looping Script that runs on Stampede

--- Loop.sh
+++ Loop.sh
@@ -1,3 +1,3 @@
 for f in $HOME/git/Protein_Analyzer/approved1PDBQT/*.pdbqt; do
-    ./autodock_vina_1_1_2_linux_x86/bin/vina --receptor protein.pdbqt --ligand $f --center_x 10 --center_y 90.5 --center_z 57.5 --size_x 22 --size_y 24 --size_z 28
+    ./autodock_vina_1_1_2_linux_x86/bin/vina --receptor protein.pdbqt --ligand $f --center_x 11 --center_y 90.5 --center_z 57.5 --size_x 22 --size_y 24 --size_z 28
 done
