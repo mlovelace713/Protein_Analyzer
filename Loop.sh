49e0ab029ade4110f17bc85bd7ec2da442bced3c
mlovelace713 <mlovelace713@gmail.com>
2017-11-05 20:47:32 -0500

    Loop script for Stampede

--- Loop.sh
+++ Loop.sh
@@ -1,3 +1,3 @@
 for f in $HOME/git/Protein_Analyzer/approved1PDBQT/*.pdbqt; do
-    ./autodock_vina_1_1_2_linux_x86/bin/vina --receptor $Home/git/Protein_Analyzer/1ceq.pdbqt --ligand $f --center_x 11 --center_y 90.5 --center_z 57.5 --size_x 22 --size_y 24 --size_z 28
+    $HOME/autodock_vina_1_1_2_linux_x86/bin/vina --receptor $HOME/git/Protein_Analyzer/1ceq.pdbqt --ligand $f --center_x 11 --center_y 90.5 --center_z 57.5 --size_x 22 --size_y 24 --size_z 28
 done
