75bdb62570dda1e69627d7d8d693adbc314b41fa
mlovelace713 <mlovelace713@gmail.com>
2017-11-05 21:22:02 -0500

    Loop.sh script with correct docking region

--- Loop.sh
+++ Loop.sh
@@ -1,3 +1,3 @@
 for f in $HOME/git/Protein_Analyzer/approved1PDBQT/*.pdbqt; do
-    $HOME/autodock_vina_1_1_2_linux_x86/bin/vina --receptor $HOME/git/Protein_Analyzer/1ceq.pdbqt --ligand $f --center_x 11 --center_y 90.5 --center_z 57.5 --size_x 22 --size_y 24 --size_z 28
+    $HOME/autodock_vina_1_1_2_linux_x86/bin/vina --receptor $HOME/git/Protein_Analyzer/1ceq.pdbqt --ligand $f --center_x 37 --center_y 11 --center_z 7 --size_x 40 --size_y 40 --size_z 40
 done
