9cf96d4403be704865eea0312d3e0ca60235017c
mlovelace713 <mlovelace713@gmail.com>
2017-11-10 10:24:04 -0500

    Create LoopNU4.sh

---
+++ Scripts/Loop/LoopNU4.sh
@@ -0,0 +1,3 @@
+for f in $HOME/git/Protein_Analyzer/Batches/Development/nutraceuticalPDBQT4/*.pdbqt; do
+    $HOME/autodock_vina_1_1_2_linux_x86/bin/vina --receptor $HOME/git/Protein_Analyzer/1ceq.pdbqt --ligand $f --center_x 37 --center_y 11 --center_z 7 --size_x 40 --size_y 40 --size_z 40
+done
